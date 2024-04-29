861344ea228de1a4f04f3167f55e468f44b3a187
petraroso <proso00@fesb.hr>
2024-04-29 23:18:27 +0200

    Activities sort by date

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -16,19 +16,52 @@ interface Activity {
 
 export default function ActivitiesPage() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [sortValue, setSortValue] = useState("latest");
 
   useEffect(() => {
     axios
       .get("http://localhost:3001/activities")
       .then((res) => {
-        setActivities(res.data);
+        setActivities(sortedActivitiesAscending(res.data));
       })
       .catch((err) => console.log(err.message));
   }, []);
 
+  const handleSort = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    setSortValue(() => value);
+
+    if (value === "latest") {
+      setActivities(sortedActivitiesAscending([...activities]));
+    } else {
+      setActivities(sortedActivitiesDescending([...activities]));
+    }
+  };
+
+  const sortedActivitiesAscending = (activities: Activity[]) => {
+    return activities.sort((a: Activity, b: Activity) => {
+      return parseDate(b.date).getTime() - parseDate(a.date).getTime();
+    });
+  };
+
+  const sortedActivitiesDescending = (activities: Activity[]) => {
+    return activities.sort((a: Activity, b: Activity) => {
+      return parseDate(a.date).getTime() - parseDate(b.date).getTime();
+    });
+  };
+
+  const parseDate = (dateString: string): Date => {
+    const [year, day, month] = dateString.split("-");
+    return new Date(`${year}-${month}-${day}`);
+  };
+
   return (
     <>
       <h2>Popis aktivnosti</h2>
+      <select id="sort" name="sort" value={sortValue} onChange={handleSort}>
+        <option value={"latest"}>Najnoviji</option>
+        <option value={"oldest"}>Najstariji</option>
+      </select>
       {activities.map((activity, index) => (
         <ActivityCard key={index} activity={activity} />
       ))}
