34104cc446a1a75320c7963717bd7fb7301e1e88
petraroso <proso00@fesb.hr>
2024-05-04 23:26:26 +0200

    New association form basic css

--- src/components/Forms/NewAssociationForm.tsx
+++ src/components/Forms/NewAssociationForm.tsx
@@ -34,6 +34,7 @@ export default function NewAssociationForm({
       formData.city === ""
     ) {
       window.alert("Unesite sve podatke.");
+      setUserMessage(false);
     } else {
       axios
         .post("http://localhost:3001/associations", formData)
@@ -48,53 +49,50 @@ export default function NewAssociationForm({
   };
 
   return (
-    <>
+    <div className="form">
       <h2>Nova udruga</h2>
-      <label>
-        Naziv:
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Naziv udruge"
-          value={formData.name}
-          onChange={handleFormData}
-        />
-      </label>
-      <label>
-        Adresa:
-        <input
-          type="text"
-          id="address"
-          name="address"
-          placeholder="Adresa udruge"
-          value={formData.address}
-          onChange={handleFormData}
-        />
-      </label>
-      <label>
-        Grad:
-        <select
-          id="city"
-          name="city"
-          value={formData.city}
-          onChange={handleFormData}
-          required
-        >
-          {cities.map((city) => (
-            <option key={city.name} value={city.name}>
-              {city.name}
-            </option>
-          ))}
-        </select>
-      </label>
+      <label htmlFor="name">Naziv:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Naziv udruge"
+        value={formData.name}
+        onChange={handleFormData}
+      />
+
+      <label htmlFor="address">Adresa:</label>
+      <input
+        type="text"
+        id="address"
+        name="address"
+        placeholder="Adresa udruge"
+        value={formData.address}
+        onChange={handleFormData}
+      />
+
+      <label htmlFor="city">Grad:</label>
+      <select
+        id="city"
+        name="city"
+        value={formData.city}
+        onChange={handleFormData}
+        required
+      >
+        {cities.map((city) => (
+          <option key={city.name} value={city.name}>
+            {city.name}
+          </option>
+        ))}
+      </select>
 
       <button onClick={sendData}>Spremi ✔️</button>
       {userMessage && (
         <span className="successMessage">
-          Zahtjev poslan! Udruga će biti dodana nakon odobrenja!
+          Zahtjev poslan!<br></br>
+          Udruga će biti dodana nakon odobrenja!
         </span>
       )}
-    </>
+    </div>
   );
 }
