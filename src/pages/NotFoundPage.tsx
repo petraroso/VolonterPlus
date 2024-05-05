8ea3b77244844b167e75ba9dfde29f869c5d8516
petraroso <proso00@fesb.hr>
2024-05-05 23:43:04 +0200

    404 page css added

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 //Link doesn't refresh the page, only routes to a page
 //It does client side routing
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 not found!</h1>
-      <Link to="/">Home</Link>
+    <div className="page-404">
+      <div className="container-404">
+        <h1>404 not found!</h1>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+      </div>
     </div>
   );
 }
